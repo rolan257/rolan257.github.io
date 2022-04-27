cfbf7778a7ea791b1b16c14aa9bcfa1967140e5d
rolan257 <rolan257@protonmail.com>
2022-04-27 14:27:45 +0200

    Rabet tests.

--- js/utils-stellar.js
+++ js/utils-stellar.js
@@ -3,6 +3,10 @@ const server = new StellarSdk.Server(gData.api.horizon);
 async function getBalances() {
   let balance, amount, hasAsset = false;
 
+  let assetsTable = $('#assets');
+  let tbody = $('#assets tbody');
+  let tr;
+
   gData.account.sAccount = await server.loadAccount(gData.account.pk);
 
   for (balance of gData.account.sAccount.balances) {
@@ -11,17 +15,26 @@ async function getBalances() {
     if (_.has(balance, 'asset_code')) {
       console.log(amount + ' ' + balance.asset_code);
 
+      tr = '<tr><td>' + amount + '</td><td>' + balance.asset_code + '</td></tr>';
+      tbody.append(tr);
+
       if ((balance.asset_code == 'DRIFT') && amount) {
         hasAsset = true;
         alert('You have ' + amount + ' DRIFT');
       }
     }
-    else
+    else {
+      tr = '<tr><td>' + amount + '</td><td>XLM</td></tr>';
+      tbody.append(tr);
+
       console.log(amount + ' XLM');
+    }
   }
 
   if (!hasAsset)
     alert('You don\'t have any DRIFT tokens.');
+
+  assetsTable.removeClass('d-none');
 
   return hasAsset;
 }
